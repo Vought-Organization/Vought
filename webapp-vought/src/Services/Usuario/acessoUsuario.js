dd5a8aa3095ec8fbfdae6ce4837f8b14ef6c2968
DerliJunior <derli.junior@bandtec.com.br>
2022-11-23 03:22:21 -0300

    Endpoint de cadastro

--- webapp-vought/src/Services/Usuario/acessoUsuario.js
+++ webapp-vought/src/Services/Usuario/acessoUsuario.js
@@ -1,8 +1,13 @@
 import { useServices } from '../../apiService';
 
-export const acessoUsuario = async () => {
-  const { users } = useServices();
+const { users } = useServices();
 
+export const acessoUsuario = async () => {
   const response = await users.get('/login');
+  return response;
+};
+
+export const cadastroUsuario = async (entrada) => {
+  const response = await users.post('/', entrada);
   return response;
 };
