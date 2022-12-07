151335971c5dd2f606a609214d55ed7c78890aa5
Eduardo Cardoso <eduardo.cardoso@VALEMOBI.CORP@VMNOTE-375.valemobi.corp>
2022-12-07 13:30:02 -0300

    refactor: import txt

--- webapp-vought/src/Pages/HotSite/index.jsx
+++ webapp-vought/src/Pages/HotSite/index.jsx
@@ -13,13 +13,25 @@ function HotSite() {
         events.get("http://localhost:8080/v1/events/export-eventos")
         .then((response) => {
             console.log(response.data);
-            // setEvento(response.data);
             const url = window.URL.createObjectURL(new Blob([response.data]))
             const link = document.createElement("a")
             link.href = url 
             link.setAttribute("download", "exportation-information.txt");
             document.body.appendChild(link)
             link.click();
+        })
+        .catch((errorOcorrido) => {
+            console.log(errorOcorrido)
+        })
+    }
+
+    async function importTxt() {
+        console.log("Requisição está sendo feita: ");
+    
+        events.get("http://localhost:8080/v1/events/import-eventos")
+        .then((response) => {
+            console.log(response.data);
+            alert("Dados Importados com sucesso!")
         })
         .catch((errorOcorrido) => {
             console.log(errorOcorrido)
@@ -35,6 +47,7 @@ function HotSite() {
                 <div className="content">
                      <h2>Explore the World</h2>
                      <button onClick={() => downloadTxt()} className="btn"> Descubra Agora</button>
+                     <button onClick={() => importTxt()} className="btn"> Importar Dados</button>
                 </div>
                 <div className="search_box">
                     <div className="input_bx">
